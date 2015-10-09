ec57a10bb9588be0892ca9daff9547d4b81c3bc6
KrInMotion <kr.inmotion@gmail.com>
2015-10-09 14:43:56 +0300

    Added category list viewmodel and view

---
+++ Web/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.ViewModels;
+
+namespace Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            var model = new List<CategoryViewModel>
+            {
+                new CategoryViewModel {Id=1, CategoryName="Категория №1", RecipeCount=0 },
+                new CategoryViewModel {Id=2, CategoryName="Категория №2", RecipeCount=10, DeleteClass="disabled" },
+                new CategoryViewModel {Id=3, CategoryName="Категория №3", RecipeCount=4, DeleteClass="disabled" },
+                new CategoryViewModel {Id=4, CategoryName="Категория №4", RecipeCount=6, DeleteClass="disabled" }
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Web/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace Web.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        [DisplayName("Наименование категории")]
+        public string CategoryName { get; set; }
+        [DisplayName("Кол-во рецептов")]
+        public int RecipeCount { get; set; }
+        public string DeleteClass { get; set; }
+    }
+}
